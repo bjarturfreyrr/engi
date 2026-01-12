14427a4d4c2641b7642268dcfaf5e8ed3a3b520d
Bjartur Freyr Bjarnason <bjarturfreyrbjarnason@Bjarturs-MacBook-Air.local>
2026-01-12 15:37:31 +0000

    Vefsidu section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,39 @@
 
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import Image from 'next/image'
+import Link from 'next/link'
+
+const websites = [
+  {
+    title: "Chad Grooming",
+    desc: "Netverslun á karlmans rakstursvörum",
+    page: "/",
+    image: "/chadgrooming2.png",
+  },
+  {
+    title: "Kr Law",
+    desc: "Lögfræðistofa",
+    page: "/",
+    image: "/krlaw.png",
+  },
+  {
+    title: "Arctic Media",
+    desc: "Markaðsstofa",
+    page: "/",
+    image: "/arctic.png",
+  }
+]
 
 const page = () => {
   return (
     <div className='w-full'>
-      <section className='min-h-screen flex items-center justify-center px-4'>
-        <div className='w-full max-w-8xl mx-auto'>
+      <section className='min-h-screen flex items-center justify-center px-30 mt-20'>
+        <div className='w-full max-w-full mx-auto'>
           <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 gap-10 lg:p-32'>
             <div className='flex flex-col gap-14 text-center lg:text-left'>
               <h1 className='text-8xl font-bold'>Stafrænar lausnir <span className='text-blue-500 font-light'>fyrir þitt fyrirtæki</span></h1>
-              <p className='text-gray-600 text-xl'>Við sjáum um vefþróun, </p>
+              <p className='text-gray-600 text-xl'>Við sjáum um vefþróun, vefsíðugerð og umsjón. </p>
               <Button className='mt-4 w-[200px] h-[50px]' variant='glass'>Skoða meira</Button>
             </div>
             <div className='flex items-center justify-center'>
@@ -20,6 +43,32 @@ const page = () => {
         </div>
       </section>
 
+
+      {/* PROJECTS */}
+      <section className='w-full flex flex-col items-center gap-15 py-20 px-30'>
+            {websites.map((item, index) => (
+              <Link href={item.page} key={index} className='w-full max-w-6xl mx-auto'>
+                <div className='flex flex-row gap-15 justify-between px-10'>
+                  <Image 
+                    src={item.image}
+                    alt={item.title}
+                    width={450}
+                    height={450}
+                    className='rounded-xl'
+                  />
+
+                  <div className='flex-1'>
+                    <h1 className='text-xl text-engi-navy-100'>{item.title}</h1>
+                    <p className='text-3xl text-engi-navy-100'>{item.desc}</p>
+                  </div>
+
+                  <div>
+                    <Button variant="glass" className='px-6 py-6'>Skoða Verkefni</Button>
+                  </div>
+                </div>
+              </Link>
+            ))}
+      </section>
     </div>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,6 +4,7 @@ import { Search } from "lucide-react"
 import { LayoutDashboard } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { ScrollProgress } from "@/components/ui/scroll-progress"
 
 const Items = [
     {
@@ -27,7 +28,7 @@ const Items = [
 
 const NavBar = () => {
   return (
-    <div className='grid grid-cols-3 items-center rounded-full m-10 h-[70px] px-5'>
+    <div className='grid grid-cols-3 fixed z-60 top-0 left-0 right-0 items-center h-[100px] px-10 py-5 bg-white/20 backdrop-blur-md'>
         <div className='justify-self-start items-center align-center ml-10 text-2xl text-white font-bold'>
             <Image
             src="/svg/multi-navy.svg"
@@ -42,7 +43,7 @@ const NavBar = () => {
             
                 {Items.slice(0, 3).map((item) => (
                     <Link 
-                        className="cursor-pointer" 
+                        className="cursor-pointer relative after:absolute after:-bottom-[8px] after:left-1/2 after:h-[1px] after:w-0 after:bg-green-500 after:-translate-x-1/2 after:transition-all after:duration-300 hover:after:w-full" 
                         href={item.href} 
                         key={item.name}>
                         {item.name}
@@ -62,6 +63,8 @@ const NavBar = () => {
             <Search className="w-[25px] h-[25px] font-light cursor-pointer text-engi-navy-100" />
             <Button className="w-[200px] h-[50px]" variant="glass">Skoða meira</Button>
         </div>
+
+        <ScrollProgress className="top-[100px]" />
     </div>
   )
 }
